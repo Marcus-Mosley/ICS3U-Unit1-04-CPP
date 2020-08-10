d65843f5ca62d7095ca1591fa042810df5234f47
Marcus-Mosley <68971666+Marcus-Mosley@users.noreply.github.com>
2020-08-10 17:04:28 -0400

    Update area_and_perimeter.cpp

--- area_and_perimeter.cpp
+++ area_and_perimeter.cpp
@@ -11,6 +11,6 @@ int main()  {
     std::cout << "If a rectangle has the dimensions:" << std::endl;
     std::cout << "5cm x 3cm" << std::endl;
     std::cout << "" << std::endl;
-    std::cout << "Area is " << (3*5) << "cm^2" << std::endl;
-    std::cout << "Perimeter is " << (2*(3+5)) << "cm" << std::endl;
+    std::cout << "Area is " << (5*3) << "cm^2" << std::endl;
+    std::cout << "Perimeter is " << (2*(5+3)) << "cm" << std::endl;
 }
